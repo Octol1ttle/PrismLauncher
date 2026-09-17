b3378be576bc33dd6b088bbdf0fd0b7eecd6f9a4
Octol1ttle <l1ttleofficial@outlook.com>
2026-09-17 21:05:30 +0500

    tmp
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- .github/scripts/labels.js
+++ .github/scripts/labels.js
@@ -2,5 +2,5 @@ module.exports = async ({github, context, core}) => {
     console.log('in script')
     const pr_number = process.env.PR_NUMBER
     console.log('the pr number is')
-    console.log(PR_NUMBER)
+    console.log(pr_number)
 }
