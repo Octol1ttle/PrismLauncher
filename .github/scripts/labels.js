7ecd100927b49d77e455cdf81f25ffb0ad1dc721
Octol1ttle <l1ttleofficial@outlook.com>
2026-09-17 22:20:55 +0500

    tmp
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- .github/scripts/labels.js
+++ .github/scripts/labels.js
@@ -7,8 +7,8 @@ module.exports = async ({github, context, core}) => {
 
 async function checkPullsForConflicts(github, context) {
     for await (const pull of github.paginate.iterator(github.rest.pulls.list, {
-        owner: context.repository.owner,
-        repo: context.repository.name,
+        owner: context.payload.repository.owner,
+        repo: context.payload.repository.name,
         state: 'open'
     })) {
         console.log(pull.mergeable)
